d3d4469dd739dc5fe79f7101fa2d0903dbc91df0
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-06-24 18:57:03 +0700

    add create, update relation mutation

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,12 +4,15 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeRepository: Repository<Coffee>,
+    @InjectRepository(Flavor)
+    private readonly flavorRepository: Repository<Flavor>,
   ) {}
 
   findAll() {
@@ -36,15 +39,29 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createCoffeDto: CreateCoffeeDto) {
-    const coffee = this.coffeRepository.create(createCoffeDto);
+  async create(createCoffeDto: CreateCoffeeDto) {
+    const flavors = await Promise.all(
+      createCoffeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+    );
+
+    const coffee = this.coffeRepository.create({
+      ...createCoffeDto,
+      flavors,
+    });
     return this.coffeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeDto: UpdateCoffeeDto) {
+    const flavors =
+      updateCoffeDto.flavors &&
+      (await Promise.all(
+        updateCoffeDto.flavors.map((name) => this.preloadFlavorByName(name)),
+      ));
+
     const coffee = await this.coffeRepository.preload({
       id: +id,
       ...updateCoffeDto,
+      flavors,
     });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -56,5 +73,19 @@ export class CoffeesService {
     const coffee = await this.findOne(id);
 
     return this.coffeRepository.remove(coffee);
+  }
+
+  private async preloadFlavorByName(name: string): Promise<Flavor> {
+    const existingFlavor = await this.flavorRepository.findOne({
+      where: {
+        name,
+      },
+    });
+    if (existingFlavor) {
+      return existingFlavor;
+    }
+    return this.flavorRepository.create({
+      name,
+    });
   }
 }
