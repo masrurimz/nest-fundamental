983fa0a14031c5e2a99c5db299a596fef016896d
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-06-25 15:22:40 +0700

    migration: rename coffee column "name" to "title"

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+});
+
+export default dataSource;
