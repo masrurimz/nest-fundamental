e74575aa4704b2c613e60fd5f2fa99b0696a0ea8
Muhammad Zahid Masruri <masruri03@gmail.com>
2022-06-28 11:02:58 +0700

    guards: config public routes using metadata

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 import { APP_PIPE } from '@nestjs/core';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { APP_PIPE } from '@nestjs/core';
     }),
     CoffeesModule,
     CoffeeRatingModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [
